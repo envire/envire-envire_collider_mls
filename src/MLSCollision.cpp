18098ff99f8855a2d09571316c2f05e388a3c0d0
Yong-Ho Yoo <yong-ho.yoo@dfki.de>
2016-04-04 15:40:54 +0200

    added unit test

--- src/MLSCollision.cpp
+++ src/MLSCollision.cpp
@@ -5,72 +5,6 @@
 
 using namespace envire::collision;
 
-/*
-void MLSCollision::dCreateMLSData(const boost::shared_ptr< envire::MLSGrid >& mls)                                 
-{
-    dUASSERT( d, "argument not Mlsfield data" );
-    dIASSERT( pCallback );
-    dIASSERT( widthSamples >= 2 );	// Ensure we're making something with at least one cell.
-    dIASSERT( depthSamples >= 2 );
-
-    // callback
-    d->m_nGetHeightMode = 0;
-    d->m_pUserData = pUserData;
-    d->m_pGetHeightCallback = pCallback;
-    dIASSERT( d->m_pUserData == pUserData );    
-    
-    (*pCallback)(pUserData, 1, 1);
-    (*d->m_pGetHeightCallback)(d->m_pUserData, 2, 2);    
-
-    // set info
-    SetData( widthSamples, depthSamples, width, depth, scale, offset, thickness, bWrap );
-
-    // default bounds
-    d->m_fMinHeight = -dInfinity;
-    d->m_fMaxHeight = dInfinity;
-}
-void MLSCollision::SetData( int nWidthSamples, int nDepthSamples,
-                                dReal fWidth, dReal fDepth,
-                                dReal fScale, dReal fOffset, dReal fThickness,
-                                int bWrapMode )
-{
-    dIASSERT( fWidth > REAL( 0.0 ) );
-    dIASSERT( fDepth > REAL( 0.0 ) );
-    dIASSERT( nWidthSamples > 0 );
-    dIASSERT( nDepthSamples > 0 );
-
-    // x,z bounds
-    m_fWidth = fWidth;
-    m_fDepth = fDepth;
-
-    // cache half x,z bounds
-    m_fHalfWidth = fWidth / REAL( 2.0 );
-    m_fHalfDepth = fDepth / REAL( 2.0 );
-
-    // scale and offset
-    m_fScale = fScale;
-    m_fOffset = fOffset;
-
-    // infinite min height bounds
-    m_fThickness = fThickness;
-
-    // number of vertices per side
-    m_nWidthSamples = nWidthSamples;
-    m_nDepthSamples = nDepthSamples;
-
-    m_fSampleWidth = m_fWidth / ( m_nWidthSamples - REAL( 1.0 ) );
-    m_fSampleDepth = m_fDepth / ( m_nDepthSamples - REAL( 1.0 ) );
-
-    m_fSampleZXAspect = m_fSampleDepth / m_fSampleWidth;
-
-    m_fInvSampleWidth = REAL( 1.0 ) / m_fSampleWidth;
-    m_fInvSampleDepth = REAL( 1.0 ) / m_fSampleDepth;
-
-    // finite or repeated terrain?
-    m_bWrapMode = bWrapMode;
-}
-*/
-
 int MLSCollision::dCollideMlsfieldZone( const boost::shared_ptr<envire::MLSGrid>& mls,
 										   const int minX, const int maxX, const int minZ, const int maxZ, 
                                            dxGeom* o2, const int numMaxContactsPossible,
@@ -96,32 +30,13 @@ int MLSCollision::dCollideMlsfieldZone( const boost::shared_ptr<envire::MLSGrid>
 }
 
 int MLSCollision::collide(dGeomID o1, dGeomID o2, int flags, dContactGeom* contact, int skip, const boost::shared_ptr< envire::MLSGrid >& mls, int o2_class_id)
-{
-	/*
-	//size_t cellSizeX, cellSizeY;
-	//double scalex, scaley;	
-	//REAL( 1.0 ) / m_fSampleWidth;
-    unsigned long size;
-    int x, y;
-    //int height, width;
-    height =140;
-    width =140;       
-    //dReal targetWidth,targetHeight, scale;
-    targetWidth = 14.0;
-    targetHeight = 14.0;
-    scale = 1.0;
-    */
-			o2->computeAABB();
+{		
+		o2->computeAABB();
      // To narrow scope of following variables
-        const dReal fInvSampleWidth = REAL(1.0) / mls->getScaleX();//terrain->m_p_data->m_fInvSampleWidth;
+        const dReal fInvSampleWidth = REAL(1.0) / mls->getScaleX();
         int nMinX = (int)dFloor(dNextAfter(o2->aabb[0] * fInvSampleWidth, -dInfinity));
-     std::cout << "\n nMinX, o2->aabb[0], fInvSampleWidth, -dInfinity : " << nMinX <<","<<
-      o2->aabb[0] <<","<< fInvSampleWidth<<","<< -dInfinity<<std::endl;       
         int nMaxX = (int)dCeil(dNextAfter(o2->aabb[1] * fInvSampleWidth, dInfinity));
-     std::cout << "\n getCellSizeX, type nMaxX, o2->aabb[1], fInvSampleWidth, dInfinity : " << mls->getScaleX() <<"," <<nMaxX <<","<<
-      o2->aabb[1] <<","<< fInvSampleWidth<<","<< dInfinity<<std::endl;    
-              
-        const dReal fInvSampleDepth = REAL( 1.0 ) / mls->getScaleY();//terrain->m_p_data->m_fInvSampleDepth;
+        const dReal fInvSampleDepth = REAL( 1.0 ) / mls->getScaleY();
         int nMinZ = (int)dFloor(dNextAfter(o2->aabb[4] * fInvSampleDepth, -dInfinity));
         int nMaxZ = (int)dCeil(dNextAfter(o2->aabb[5] * fInvSampleDepth, dInfinity));
 
@@ -140,9 +55,9 @@ int MLSCollision::collide(dGeomID o1, dGeomID o2, int flags, dContactGeom* conta
 		int numMaxTerrainContacts = 30;    
         
         numTerrainOrigContacts = numTerrainContacts;
-        //numTerrainContacts += dCollideMlsfieldZone(
-            //nMinX,nMaxX,nMinZ,nMaxZ,o2,numMaxTerrainContacts - numTerrainContacts,
-            //flags,CONTACT(contact,numTerrainContacts*skip),skip	);
+        numTerrainContacts += dCollideMlsfieldZone(mls,
+            nMinX,nMaxX,nMinZ,nMaxZ,o2,numMaxTerrainContacts - numTerrainContacts,
+            flags,CONTACT(contact,numTerrainContacts*skip),skip	);
       
         dIASSERT( numTerrainContacts <= numMaxTerrainContacts );
     
---
+++ test/test_mlscollider.cpp
@@ -0,0 +1,84 @@
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <ode/collision.h>
+#include <ode/common.h>
+#include <ode/rotation.h>
+#include <ode/matrix.h>
+#include <ode/odemath.h>
+#include <envire_collision/collision_kernel.h>
+#include <envire_collision/collision_std.h>
+#include <envire_collision/collision_util.h>
+#include <envire_collision/config.h>
+
+#include <envire_core/items/Item.hpp>
+#include <envire_collision/Exceptions.hpp>
+
+#include <envire_collider_mls/MLSCollision.hpp>
+
+
+#include <envire/Core.hpp>
+#include <envire/maps/GridBase.hpp>
+#include <envire/maps/MLSGrid.hpp>
+#include <envire/operators/MLSToGrid.hpp>
+#include <envire/operators/MLSToMLSGeom.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/intrusive_ptr.hpp>
+
+using namespace envire::collision;
+using namespace envire;
+
+struct NullDeleter {template<typename T> void operator()(T*) {}};
+
+BOOST_AUTO_TEST_CASE(test_box_collision)
+{
+
+    dInitODE();
+    {
+		
+		
+        MLSCollision* c = MLSCollision::getInstance();
+        BOOST_CHECK(c != NULL);
+        BOOST_CHECK(c->getGeomID() == -1);
+        
+		std::string env_path("mls_data");
+		std::string mls_map_id("/mls-grid");
+
+			
+		boost::scoped_ptr<envire::Environment> env(envire::Environment::unserialize(env_path));   
+		envire::MLSGrid::Ptr mlsgrid_ptr(env->getItem<envire::MLSGrid>(mls_map_id));		
+		boost::shared_ptr<envire::MLSGrid> mls(mlsgrid_ptr.get(), NullDeleter());
+	
+		//printf("mls.cellSizeX = (%d %d)\n",	mls->getCellSizeX(),	mls->getCellSizeY());
+
+        // create first geom
+        dGeomID geom_mls = c->createNewCollisionObject(mls);
+        //c->setTransformation(geom_a, Eigen::Affine3d::Identity());
+
+        // check interface
+        BOOST_CHECK(c->getGeomID() >= dFirstUserClass);
+        BOOST_CHECK(dGeomGetClass(geom_mls) == c->getGeomID());
+
+        boost::shared_ptr<envire::MLSGrid> user_data_a2 = c->getUserData(geom_mls);
+
+        //BOOST_CHECK(user_data_a.use_count() == 3);
+        BOOST_CHECK(mls == user_data_a2);
+
+        // create second geom
+        dxSphere* geom_sphere = new dxSphere(0, 1);
+		dGeomSetPosition (geom_sphere,0,0,0);        
+        
+        for(int i=1;i<100;i++)
+        {
+
+       // dGeomSetPosition (geom_sphere,0,0,0-0.1*(float)i);
+                
+        dContactGeom cg[10];
+        int points = dCollide(geom_mls, geom_sphere, 1,  &cg[0], sizeof cg[0]);  
+        printf("(NumOfCollision %d) (%f %f %f)\n",points,cg[0].pos[0],cg[0].pos[1],cg[0].pos[2]);
+		}        
+        dGeomDestroy(geom_mls);
+        dGeomDestroy(geom_sphere);
+
+    }
+    dCloseODE();
+}
